06e3877c3fd8cea5309b9de2dcbcbec922e2c13d
johngarcia73 <129740948+johngarcia73@users.noreply.github.com>
2024-09-23 15:58:16 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import tkinter as tk
 from tkinter import Menu, Frame, Canvas, Scrollbar, Checkbutton, IntVar
 
+# Parametros globales
+numNodosDefault = 100       
+probAristasDefault = 0.2
+
+probAristas = probAristasDefault
+numeroNodos = numNodosDefault
+
 def on_enter(e):
     e.widget.config(bg='lightgrey')
 
@@ -44,6 +51,43 @@ def deselect_all():
         var.set(0)
     update_sections()
 
+def guardar_valor(valor, opcion):
+    if(opcion == 1):
+        global numeroNodos
+        numeroNodos = int(float(valor))
+        if(numeroNodos < 0): numeroNodos = 0
+        if(numeroNodos > 1000): numeroNodos = 1000
+        print(f"Valor guardado, nodos: {numeroNodos}")
+    if(opcion == 2):
+        global probAristas
+        probAristas = float(valor)/100
+        if(probAristas < 0): probAristas = 0
+        if(probAristas > 1): probAristas = 1
+        print(f"Valor guardado, probabilidad de aristas: {probAristas}")
+    
+def ventana_def_numero(encabezado, lim_inf, lim_sup, opcion, inc):
+    miniventana = tk.Toplevel(root)
+    miniventana.title("Seleccionar valor")
+    miniventana.geometry("300x150")
+    
+    label = tk.Label(miniventana, text=encabezado)
+    label.pack(pady=10)
+    
+    spinbox = tk.Spinbox(miniventana, from_=lim_inf, to=lim_sup, increment=inc)
+    spinbox.pack(pady=10)
+    
+    def guardar():
+        valor = spinbox.get()
+        guardar_valor(valor, opcion)
+        miniventana.destroy()
+    
+    boton_aceptar = tk.Button(miniventana, text="Aceptar", command=guardar)
+    boton_aceptar.pack(pady=10)
+    
+def mostrar_grafo():
+    print("Ahora se mostraría el grafo")
+    
+       
 # Crear la ventana principal
 root = tk.Tk()
 root.geometry("820x540")
@@ -56,14 +100,23 @@ menu_bar = tk.Frame(root, bg='white')
 menu_bar.pack(side=tk.TOP, fill=tk.X)
 
 # Crear los apartados del menú
-apartado1 = create_menu_item(menu_bar, "Apartado 1")
+apartado1 = create_menu_item(menu_bar, "Personalizar grafo")
 apartado2 = create_menu_item(menu_bar, "Apartado 2")
 apartado3 = create_menu_item(menu_bar, "Parámetros")
 
+boton_label = tk.Label(menu_bar, text="Mostrar grafo", padx=10, pady=5)
+boton_label.pack(side=tk.LEFT)
+
+# Añadir eventos para el "botón"
+boton_label.bind("<Enter>", on_enter)
+boton_label.bind("<Leave>", on_leave)
+boton_label.bind("<Button-1>", lambda e: mostrar_grafo())
+
 # Crear los menús desplegables
 submenu1 = Menu(root, tearoff=0)
-submenu1.add_command(label="Opción 1.1")
-submenu1.add_command(label="Opción 1.2")
+submenu1.add_command(label="Número de nodos", command=lambda: ventana_def_numero("Seleccione el número de nodos:", 0, 1000, 1, 1))
+submenu1.add_command(label="Probabilidad de conexión", command=lambda: ventana_def_numero("Seleccione la probabilidad de arista entre dos nodos (%):", 0.0, 100.0, 2, 0.5))
+submenu1.add_command(label="Crear grafo aleatorio")
 
 submenu2 = Menu(root, tearoff=0)
 submenu2.add_command(label="Opción 2.1")
